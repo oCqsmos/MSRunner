d7aaa8075b40bc9654e87172b353f52e37591a3c
oCqsmos <ninja.sushi49@gmail.com>
2024-03-29 20:50:42 -0700

    shell of workings

--- main_code.py
+++ main_code.py
@@ -1,1 +1,28 @@
-print("Hello world")
+# from tkinter import *
+# from tkinter import messagebox
+# def gridadd(kind,gridx,gridy):
+#     if kind=="bomb":
+#         grid[gridy].append(Button(root,text=))
+
+# root = Tk()
+# root.title('MineSweeper')
+# roo2=Tk()
+# roo2.title('GameText')
+
+# global grid
+# grid=[[0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0],
+#       [0,0,0,0,0,0,0,0,0,0]]
+# textupdates=[]
+
+
+# root.mainloop()
+
+print("hello world")
